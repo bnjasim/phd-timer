86d315aff4fc7023b72c40d34798a0ed08fcb3a2
Binu Jasim <bnjasim@gmail.com>
2017-01-03 12:12:06 +0530

    Basic timer handler

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+# @author Binu Jasim
+# @created on 03-Jan-2017
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello PhD!')
+import webapp2
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
+								autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+	
+	def render_str(self, template, **params):
+		try:
+			return (jinja_env.get_template(template)).render(params)
+		except:
+			return (jinja_env.get_template('blog/blog-error.html')).render()
+
+	def render(self, template, **html_add_ins):
+		self.write(self.render_str(template, **html_add_ins))
+	
+
+class TimerHandler(Handler):
+	def get(self):	
+		self.render('timer.html')		
+		#self.response.out.write(url)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+
+    ('/', TimerHandler)
 ], debug=True)
