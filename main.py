e3baaaa323cf5d709e4bcf8ada49700f25c4d194
Binu Jasim <bnjasim@gmail.com>
2017-01-05 08:59:24 +0530

    Define Entity models - Entry and Work

--- main.py
+++ main.py
@@ -5,10 +5,48 @@
 import webapp2
 import os
 import jinja2
+from google.appengine.ext import ndb
+from google.appengine.api import users
+import logging
+import datetime
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
 								autoescape = True)
+
+# username is not Id because users might change their user name or email
+class Account(ndb.Model):
+	nickname = ndb.StringProperty()
+	email = ndb.StringProperty()
+	userCreated = ndb.DateTimeProperty(auto_now_add = True)
+
+	@classmethod
+	@ndb.transactional
+	def my_get_or_insert(cls, id, **kwds):
+		key = ndb.Key(cls, id)
+		ent = key.get()
+		if ent is not None:
+			return False  # False meaning "Already existing"
+		ent = cls(**kwds)
+		ent.key = key
+		ent.put()
+		return True  # True meaning "Newly created"
+	
+class Entry(ndb.Model):
+	# The parent of an Entry is Account
+	date = ndb.DateProperty(required = True)	
+	hours = ndb.IntegerProperty()
+	mins = ndb.IntegerProperty()
+	notes = ndb.TextProperty()
+
+# Only a single instance of this Entity type is required.
+# Should have found other way! But keeping in client side or in memcache don't appear good
+class Work(ndb.Model):
+	isWorking = ndb.BooleanProperty(default = False)
+	start = ndb.DateTimeProperty()
+	totalh = ndb.IntegerProperty()
+	totalm = ndb.IntegerProperty()
+
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
